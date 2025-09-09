83775911bf90b62bd658362930452de1235bc26e
hannalee2 <hanna.lee@alleninstitute.org>
2025-09-09 11:06:51 -0700

    Update dynamic version

--- efficient_track_anything/__init__.py
+++ efficient_track_anything/__init__.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+__version__ = "0.1.0"
+
 from hydra import initialize_config_module
 from hydra.core.global_hydra import GlobalHydra
 
